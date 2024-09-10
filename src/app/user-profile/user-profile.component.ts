d81331b02c53cad3de254eb29907ee9d291564a5
rasyahroel <rasyahroel132@gmail.com>
2024-09-10 10:03:32 +0700

    FIX (View): Update to Responsive

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TabViewModule } from 'primeng/tabview';
+import { OrdersComponent } from './orders/orders.component';
+import { ProfilesComponent } from './profiles/profiles.component';
+import { VouchersComponent } from './vouchers/vouchers.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [MatTabsModule, MatCardModule, CommonModule, TabViewModule, OrdersComponent, VouchersComponent, ProfilesComponent],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
---
+++ src/app/user-profile/vouchers/vouchers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vouchers',
+  standalone: true,
+  imports: [],
+  templateUrl: './vouchers.component.html',
+  styleUrl: './vouchers.component.css'
+})
+export class VouchersComponent {
+
+}
